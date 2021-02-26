b17edd34d1d169fe3c904ba54c525116172e5aef
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2021-02-26 17:38:48 -0500

    Add function to create html row for salesperson

--- src/js/salespeople.js
+++ src/js/salespeople.js
@@ -8,4 +8,42 @@ const createSalespersonTable = (salespeople) => {
 const createSalespersonRow = (salesperson) => {
 	console.log(salesperson.name);
 }
+const createSalespersonRow = (salesperson) => {
+	let row = document.createElement("tr")
+	let columnOne = document.createElement("td")
+	let columnTwo = document.createElement("td")
+	let columnThree = document.createElement("td")
+
+	let image = document.createElement("img")
+	image.setAttribute("src", salesperson.photo)
+
+	let name = document.createElement("span")
+	name.appendChild(document.createTextNode(salesperson.name))
+	name.classList.add("salesperson-name")
+
+	let deals = document.createElement("span")
+	deals.appendChild(document.createTextNode(`${salesperson.dealsClosed} closed deals`))
+	deals.classList.add("closed-deals")
+
+	let profileLink = document.createElement("a")
+	profileLink.setAttribute("href", `/salespeople/${salesperson.id}`)
+	profileLink.setAttribute("title", `View profile of ${salesperson.name}`)
+	profileLink.appendChild(document.createTextNode("View profile"))
+
+	let messageLink = document.createElement("a")
+	messageLink.setAttribute("href", `/message/user/${salesperson.id}`)
+	messageLink.setAttribute("title", `Message ${salesperson.name}`)
+	messageLink.appendChild(document.createTextNode("Message"))
+
+	columnOne.appendChild(image)
+	columnOne.appendChild(name)
+	columnTwo.appendChild(deals)
+	columnThree.appendChild(profileLink)
+	columnThree.appendChild(messageLink)
+
+	row.appendChild(columnOne)
+	row.appendChild(columnTwo)
+	row.appendChild(columnThree)
+
+	return row
 }
