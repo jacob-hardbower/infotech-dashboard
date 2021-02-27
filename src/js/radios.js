7975b97af3a1a56e1d591bd4c8998b431c0e981a
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2021-02-27 12:19:25 -0500

    Refactor radio code into single file

--- src/js/radios.js
+++ src/js/radios.js
@@ -28,3 +28,20 @@ const registerListingRadios = salespeople => {
 		}
 	}
 }
+
+/**
+ * Creates radio buttons and handlers for filtering leaderboard
+ *
+ * @param {array} salespeople Array containing salesperson objects
+ */
+const registerPeriodRadios = salespeople => {
+	const periodRadios = document.getElementsByName('period')
+
+	const repopulate = period => populateLeaderboard(salespeople, period)
+
+	for(let i = 0; i < periodRadios.length; i++) {
+	  periodRadios[i].onclick = () => {
+			repopulate(periodRadios[i].value)
+		}
+	}
+}
