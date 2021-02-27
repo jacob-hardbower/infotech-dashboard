054216e588e6a193f95307d19be671417d4f85b8
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2021-02-27 11:35:25 -0500

    Add topPerformer data

--- src/data/salespeople.js
+++ src/data/salespeople.js
@@ -6,109 +6,199 @@ const getSalesPeople = () => {
 			id: 1,
 			name: "Abdullah Hadley",
 			photo: "https://randomuser.me/api/portraits/men/4.jpg",
-			dealsClosed: 122
+			dealsClosed: 122,
+			topPerformer: {
+				day: true,
+				week: true,
+				month: true
+			}
 		},
 		{
 			id: 2,
 			name: "Renee Sims",
 			photo: "https://randomuser.me/api/portraits/women/4.jpg",
-			dealsClosed: 102
+			dealsClosed: 102,
+			topPerformer: {
+				day: true,
+				week: false,
+				month: true
+			}
 		},
 		{
 			id: 3,
 			name: "Carys Metz",
 			photo: "https://randomuser.me/api/portraits/women/6.jpg",
-			dealsClosed: 90
+			dealsClosed: 90,
+			topPerformer: {
+				day: true,
+				week: true,
+				month: false
+			}
 		},
 		{
 			id: 4,
 			name: "Eduard Franz",
 			photo: "https://randomuser.me/api/portraits/men/6.jpg",
-			dealsClosed: 89
+			dealsClosed: 89,
+			topPerformer: {
+				day: false,
+				week: true,
+				month: false
+			}
 		},
 		{
 			id: 5,
 			name: "Alexis Doreau",
 			photo: "https://randomuser.me/api/portraits/men/7.jpg",
-			dealsClosed: 88
+			dealsClosed: 88,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: true
+			}
 		},
 		{
 			id: 6,
 			name: "Mia Denys",
 			photo: "https://randomuser.me/api/portraits/women/8.jpg",
-			dealsClosed: 85
+			dealsClosed: 85,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 7,
 			name: "Lourdes Browning",
 			photo: "https://randomuser.me/api/portraits/women/10.jpg",
-			dealsClosed: 80
+			dealsClosed: 80,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 8,
 			name: "Zachary Albright",
 			photo: "https://randomuser.me/api/portraits/men/8.jpg",
-			dealsClosed: 79
+			dealsClosed: 79,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 9,
 			name: "Tim Schoch",
 			photo: "https://randomuser.me/api/portraits/men/9.jpg",
-			dealsClosed: 78
+			dealsClosed: 78,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 10,
 			name: "Oscar Thomsen",
 			photo: "https://randomuser.me/api/portraits/men/10.jpg",
-			dealsClosed: 75
+			dealsClosed: 75,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 11,
 			name: "Vivianna Kiser",
 			photo: "https://randomuser.me/api/portraits/women/11.jpg",
-			dealsClosed: 70
+			dealsClosed: 70,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 12,
 			name: "Elena Satine",
 			photo: "https://randomuser.me/api/portraits/women/12.jpg",
-			dealsClosed: 64
+			dealsClosed: 64,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 13,
 			name: "Elliot Nolten",
 			photo: "https://randomuser.me/api/portraits/men/12.jpg",
-			dealsClosed: 60
+			dealsClosed: 60,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 14,
 			name: "Daniela Dewitt",
 			photo: "https://randomuser.me/api/portraits/women/13.jpg",
-			dealsClosed: 58
+			dealsClosed: 58,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 15,
 			name: "Chloe Sirko",
 			photo: "https://randomuser.me/api/portraits/women/14.jpg",
-			dealsClosed: 55
+			dealsClosed: 55,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 16,
 			name: "Kaine Hudson",
 			photo: "https://randomuser.me/api/portraits/men/16.jpg",
-			dealsClosed: 50
+			dealsClosed: 50,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 17,
 			name: "Stephane Moreau",
 			photo: "https://randomuser.me/api/portraits/men/21.jpg",
-			dealsClosed: 40
+			dealsClosed: 40,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		},
 		{
 			id: 18,
 			name: "Kelly Williams",
 			photo: "https://randomuser.me/api/portraits/men/29.jpg",
-			dealsClosed: 35
+			dealsClosed: 35,
+			topPerformer: {
+				day: false,
+				week: false,
+				month: false
+			}
 		}
 	]
 }
