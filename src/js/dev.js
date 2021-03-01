30b06b95ba0cbe845ee20d165b5d0810d39f91af
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2021-03-01 07:06:51 -0500

    Remove log

--- src/js/dev.js
+++ src/js/dev.js
@@ -3,7 +3,6 @@
 const disableLinks = () => {
 	const links = document.getElementsByTagName("a")
 	for(let i = 0; i < links.length; i++) {
-		console.log(links[i])
 		links[i].onclick = (e) => {
 			e.preventDefault()
 		}
